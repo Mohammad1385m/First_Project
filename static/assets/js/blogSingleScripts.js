fa6d99c4fa98c0f0f35c3a7e70e377ef39a0f35a
Mohammad1385m <mhmd.85.mohammad@gmail.com>
2025-12-11 10:06:06 +0330

    comment section is complete. site settings and slider models are created.

---
+++ static/assets/js/blogSingleScripts.js
@@ -0,0 +1,47 @@
+// const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+// $.ajaxSetup({
+//     headers: {"X-CSRFToken": csrftoken}
+// });
+let textarea = document.querySelector("#comment_text")
+
+function send_comment(blog_id) {
+    // event.preventDefault();
+    let text = textarea.value
+    $.post("/blog/send-comment/", {
+        blog_id: blog_id,
+        text: text
+    }).done(function (res) {
+            // if (res.status === "comment created") {
+            let comments_section =
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "کامنت شما بعد از تایید اضافه خواهد شد",
+                }).then(() => {
+                    $("#comments").html(res);
+                    textarea.value = ""
+                });
+        }).fail(function (err) {
+        Swal.fire({
+            position: "center",
+            icon: "error",
+            title: "something went wrong!!!",
+        });
+    })
+}
+    // } else if (res.status === "error") {
+    //     Swal.fire({
+    //         position: "center",
+    //         icon: "error",
+    //         title: "something's wrong",
+    //     });
+    // } else {
+    //     Swal.fire({
+    //         position: "center",
+    //         icon: "error",
+    //         title: "something went wrong!!!",
+    //     });
+    // }
+
+
+
